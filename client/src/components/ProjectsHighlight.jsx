747ed45a4238298dbcf94ef5b4db02daf31b1177
Noah Rydén <dev@noahryden.se>
2026-06-25 15:41:19 +0200

    Horizontal scroll project highlight

--- client/src/components/ProjectsHighlight.jsx
+++ client/src/components/ProjectsHighlight.jsx
@@ -20,22 +20,26 @@ function ProjectsHighlight() {
   try {
     return (
       <div className="highlight">
-        <span className="highlight-projects">
-          {data.length && (
-            <>
-              {data.slice(0, 3).map((item, index) => (
-                <Card
-                  key={index}
-                  color={item._id}
-                  title={item.title}
-                  mainLink={`/projects/${item._id}`}
-                  tags={item.tags}
-                  thumbnail={item.thumbnail}
-                />
-              ))}
-            </>
-          )}
-        </span>
+        <div className="highlight-scroll-wrapper">
+          <div className="highlight-scroll">
+            <span className="highlight-projects">
+              {data.length && (
+                <>
+                  {data.slice(0, 3).map((item, index) => (
+                    <Card
+                      key={index}
+                      color={item._id}
+                      title={item.title}
+                      mainLink={`/projects/${item._id}`}
+                      tags={item.tags}
+                      thumbnail={item.thumbnail}
+                    />
+                  ))}
+                </>
+              )}
+            </span>
+          </div>
+        </div>
         <NavLink to="/projects" className="see-all tag-button">
           See all projects &#x1F862;
         </NavLink>
