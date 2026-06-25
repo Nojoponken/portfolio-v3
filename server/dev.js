0fc8574b8d3f8658b484e22c8215f20c460c31c9
Noah Rydén <dev@noahryden.se>
2026-06-25 12:47:15 +0200

    local dev environment up and running

--- server/dev.js
+++ server/dev.js
@@ -1,8 +1,12 @@
 import app from "./server.js";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
+import connectDB from "./config/dbConn.js";
+
 const PORT = process.env.PORT || 3500;
 
-// mongoose.connection.once("open", () => {
-//   console.log("Connected to MongoDB");
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-// });
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+});
+
+await connectDB();
--- server/routes/root.js
+++ server/routes/root.js
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 import path from "path";
+const __dirname = import.meta.dirname;
 
 router.get(/^\/(index(.html)?)?$/, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
